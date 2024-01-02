a6afe880cf5825eec72dac05f32e0b5c355ca97b
MilaCikes <milabnc13@gmail.com>
2024-01-02 18:01:52 +0100

    react

---
+++ Studenti/MilaCikes/React/Vjezba8-Props/props/src/user/UserChildren.js
@@ -0,0 +1,13 @@
+function UserChildren({name, years, children}){
+
+    return (
+        <>
+        <p>Pozdrav, moje ime je {name} i imam {years} godina.</p>
+    <p>I hobi mi je skijanje.</p>
+    </>
+    
+    ); 
+
+}
+
+export default UserChildren;
