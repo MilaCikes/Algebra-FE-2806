d850dd13453279d98de3709faddd6ba17e93c4ce
MilaCikes <milabnc13@gmail.com>
2024-01-10 18:43:25 +0100

    React

---
+++ Studenti/MilaCikes/React/Vjezba12-Events/eventi/src/user/UserChildren.js
@@ -0,0 +1,13 @@
+function UserChildren({name, years, children}){
+
+    return (
+        <>
+        <p>Pozdrav, moje ime je {name} i imam {years} godina.</p>
+    <p>A hobi mi je plivanje.</p>
+    </>
+    
+    ); 
+
+}
+
+export default UserChildren;
